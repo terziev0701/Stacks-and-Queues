92b0fa3e26d5683bfd1e499b45b30e19086875f7
Valentin Terziev <valentinterziev9701@gmail.com>
2022-05-28 22:44:07 +0300

    Add files via upload

---
+++ Simple_Calculator02.java
@@ -0,0 +1,30 @@
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Simple_Calculator02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String [] input = scanner.nextLine().split("\\s+");
+        ArrayDeque<String> arrayDeque = new ArrayDeque<>();
+
+        Collections.addAll(arrayDeque,input);
+
+
+        while (arrayDeque.size() > 1){
+            int firstNum = Integer.parseInt(arrayDeque.pop());
+            String sign = arrayDeque.pop();
+            int secondNum = Integer.parseInt(arrayDeque.pop());
+
+            if (sign.equals("+")){
+                int result = firstNum + secondNum;
+                arrayDeque.push(String.valueOf(result));
+            }else if (sign.equals("-")){
+                int result = firstNum - secondNum;
+                arrayDeque.push(String.valueOf(result));
+            }
+        }
+        System.out.println(arrayDeque.pop());
+        }
+    }
